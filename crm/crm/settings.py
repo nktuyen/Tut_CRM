bfd2d0e8a90f72e90eb27c7df25cf768706edd5a
hpe.vndev@gmail.com <hpe.vndev@gmail.com>
2024-12-27 23:18:38 +0700

    Change DB engine

--- crm/crm/settings.py
+++ crm/crm/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'crm.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'crmdb',
+        'USER': 'root',
+        'PASSWORD':'Tunk.1988',
+        'HOST': '127.0.0.1',
+        'PORT':3306
     }
 }
 
