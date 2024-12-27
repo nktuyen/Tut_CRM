78b66a2632f3a0a2c70c4f53b7d5e6eb3ed7b0e6
hpe.vndev@gmail.com <hpe.vndev@gmail.com>
2024-12-28 00:22:08 +0700

    Login logout features done

---
+++ crm/webapp/urls.py
@@ -0,0 +1,8 @@
+from . import views
+from django.urls import path
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    #path('login/', views.login_view, name='login'),
+    path('logout/', views.logout_view, name='logout'),
+]
